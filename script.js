ab0cf87fb5eb1865c2bf75108807832c39bfda19
Matthew Nguyen <145243087+Vyyvv@users.noreply.github.com>
2023-09-23 17:40:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function displayTime() {
+    var dateTime=new Date();
+    var hrs=dateTime.getHours();
+    var min=dateTime.getMinutes();
+    var sec=dateTime.getSeconds();
+    var session=document.getElementById('session');
+    if (hrs>=12){session.innerHTML='PM';}else{session.innerHTML='AM';}
+    if (hrs>12){hrs=hrs-12;}
+    if (hrs<10){hrs="0"+hrs;}
+    if (min<10){min="0"+min;}
+    if (sec<10){sec="0"+sec;}
+    document.getElementById('hours').innerHTML=hrs;
+    document.getElementById('minutes').innerHTML=min;
+    document.getElementById('seconds').innerHTML=sec;
+}
+setInterval(displayTime, 1000);
